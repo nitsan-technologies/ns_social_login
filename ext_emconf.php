81097b078081b3d5574fa805471e6e8316ca19df
Rohan Parmar <rohan.nitsan@gmail.com>
2024-08-27 16:30:10 +0530

    [TASK] Update Extension Title

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,8 @@
 <?php
 
 $EM_CONF['ns_social_login'] = [
-    'title' => 'Social Login',
-    'description' => 'Meet our all-in-one TYPO3 Social Login extension, which allows users to log in and register using various social media accounts directly from your TYPO3 frontend. With this extension, users can easily access your site using popular platforms like Facebook, Google, Twitter, LinkedIn, Instagram, OpenID, Apple, Discord, Keycloak, Slack, Telegram, Dropbox, and more. Get seamless, one-click frontend login with the TYPO3 Social Login extension. Explore the Demo, Product Page, Documentation & Support: https://t3planet.com/typo3-social-login-extension',
+    'title' => 'Social Login and Register',
+    'description' => 'Meet our all-in-one TYPO3 Social Login and Register extension, which allows users to log in and register using various social media accounts directly from your TYPO3 frontend. With this extension, users can easily access your site using popular platforms like Facebook, Google, Twitter, LinkedIn, Instagram, OpenID, Apple, Discord, Keycloak, Slack, Telegram, Dropbox, and more. Get seamless, one-click frontend login with the TYPO3 Social Login extension. Explore the Demo, Product Page, Documentation & Support: https://t3planet.com/typo3-social-login-extension',
     'category' => 'plugin',
     'author' => 'T3:Rohan Parmar, T3:Nilesh Malankiya, QA:Krishna Dhapa',
     'author_company' => 'T3Planet // NITSAN',
